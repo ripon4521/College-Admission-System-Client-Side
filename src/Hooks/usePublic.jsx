d9d6ce67e73efe539718d6b9fdd4b040ee22a8fe
ripon4521 <riponakondo4521@gmail.com>
2025-03-20 01:13:12 +0600

    updated

--- src/Hooks/usePublic.jsx
+++ src/Hooks/usePublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosPublic = axios.create({
-    baseURL:'http://localhost:5000/'
+    baseURL:'https://college-booking-server-side-one.vercel.app'
 })
 const usePublic = () => {
    return axiosPublic
--- src/pages/Home/FeaturedCollege/FeaturedCollege.jsx
+++ src/pages/Home/FeaturedCollege/FeaturedCollege.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import { axiosPublic } from "../../../Hooks/usePublic";
 
 const FeaturedCollege = ({ searchQuery }) => {
   const [collegeDetails, setCollegeDetails] = useState([]);
@@ -16,7 +17,7 @@ const FeaturedCollege = ({ searchQuery }) => {
   useEffect(() => {
     const fetchCollegeDetails = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/allCollegeData');
+        const response = await axiosPublic.get('/allCollegeData');
         setCollegeDetails(response.data);
         setLoading(false);
       } catch (err) {
